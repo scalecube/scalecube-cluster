9d05acce3f500e66e86041d361542087290d3d2b
Ronen Nachmias <ronen.hamias@gmail.com>
2018-06-30 09:28:17 +0300

    check style

--- utils/src/main/java/io/scalecube/Throwables.java
+++ utils/src/main/java/io/scalecube/Throwables.java
@@ -8,26 +8,26 @@ public final class Throwables {
 
 
   /**
-   * Propagates {@code throwable} as-is if it is an instance of {@link RuntimeException} or {@link
-   * Error}, or else as a last resort, wraps it in a {@code RuntimeException} and then propagates.
-   * <p>This method always throws an exception. The {@code RuntimeException} return type is only for
-   * client code to make Java type system happy in case a return value is required by the enclosing
-   * method. Example usage:
+   * Propagates {@code throwable} as-is if it is an instance of {@link RuntimeException} or {@link Error}, or else as a
+   * last resort, wraps it in a {@code RuntimeException} and then propagates. This method always throws an exception.
+   * The {@code RuntimeException} return type is only for client code to make Java type system happy in case a return
+   * value is required by the enclosing method. Example usage:
+   * 
    * <pre>
-   *   T doSomething() {
-   *     try {
-   *       return someMethodThatCouldThrowAnything();
-   *     } catch (IKnowWhatToDoWithThisException e) {
-   *       return handle(e);
-   *     } catch (Throwable t) {
-   *       throw Throwables.propagate(t);
-   *     }
+   * T doSomething() {
+   *   try {
+   *     return someMethodThatCouldThrowAnything();
+   *   } catch (IKnowWhatToDoWithThisException e) {
+   *     return handle(e);
+   *   } catch (Throwable t) {
+   *     throw Throwables.propagate(t);
    *   }
+   * }
    * </pre>
    *
    * @param throwable the Throwable to propagate
-   * @return nothing will ever be returned; this return type is only for your convenience, as
-   *     illustrated in the example above
+   * @return nothing will ever be returned; this return type is only for your convenience, as illustrated in the example
+   *         above
    */
   public static RuntimeException propagate(Throwable throwable) {
     propagateIfPossible(throwable);
@@ -35,17 +35,18 @@ public final class Throwables {
   }
 
   /**
-   * Propagates {@code throwable} exactly as-is, if and only if it is an instance of {@link
-   * RuntimeException} or {@link Error}.  Example usage:
+   * Propagates {@code throwable} exactly as-is, if and only if it is an instance of {@link RuntimeException} or
+   * {@link Error}. Example usage:
+   * 
    * <pre>
-   *   try {
-   *     someMethodThatCouldThrowAnything();
-   *   } catch (IKnowWhatToDoWithThisException e) {
-   *     handle(e);
-   *   } catch (Throwable t) {
-   *     Throwables.propagateIfPossible(t);
-   *     throw new RuntimeException("unexpected", t);
-   *   }
+   * try {
+   *   someMethodThatCouldThrowAnything();
+   * } catch (IKnowWhatToDoWithThisException e) {
+   *   handle(e);
+   * } catch (Throwable t) {
+   *   Throwables.propagateIfPossible(t);
+   *   throw new RuntimeException("unexpected", t);
+   * }
    * </pre>
    */
   public static void propagateIfPossible(Throwable throwable) {
@@ -55,17 +56,18 @@ public final class Throwables {
 
   /**
    * Propagates {@code throwable} exactly as-is, if and only if it is an instance of {@code
-   * declaredType}.  Example usage:
+   * declaredType}. Example usage:
+   * 
    * <pre>
-   *   try {
-   *     someMethodThatCouldThrowAnything();
-   *   } catch (IKnowWhatToDoWithThisException e) {
-   *     handle(e);
-   *   } catch (Throwable t) {
-   *     Throwables.propagateIfInstanceOf(t, IOException.class);
-   *     Throwables.propagateIfInstanceOf(t, SQLException.class);
-   *     throw Throwables.propagate(t);
-   *   }
+   * try {
+   *   someMethodThatCouldThrowAnything();
+   * } catch (IKnowWhatToDoWithThisException e) {
+   *   handle(e);
+   * } catch (Throwable t) {
+   *   Throwables.propagateIfInstanceOf(t, IOException.class);
+   *   Throwables.propagateIfInstanceOf(t, SQLException.class);
+   *   throw Throwables.propagate(t);
+   * }
    * </pre>
    */
   public static <X extends Throwable> void propagateIfInstanceOf(Throwable throwable, Class<X> declaredType) throws X {
@@ -76,11 +78,11 @@ public final class Throwables {
   }
 
   /**
-   * Returns the innermost cause of {@code throwable}. The first throwable in a
-   * chain provides context from when the error or exception was initially
-   * detected. Example usage:
+   * Returns the innermost cause of {@code throwable}. The first throwable in a chain provides context from when the
+   * error or exception was initially detected. Example usage:
+   * 
    * <pre>
-   *   assertEquals("Unable to assign a customer id", Throwables.getRootCause(e).getMessage());
+   * assertEquals("Unable to assign a customer id", Throwables.getRootCause(e).getMessage());
    * </pre>
    */
   public static Throwable getRootCause(Throwable throwable) {
