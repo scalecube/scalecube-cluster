7382779dddccaaab98de96ff1385ede3b4696bb9
Artem Vysochyn <artem.vysochyn@gmail.com>
2018-11-27 17:02:13 +0200

    Message.sender being set correctly from within inner cluster components

--- cluster/src/main/java/io/scalecube/cluster/Cluster.java
+++ cluster/src/main/java/io/scalecube/cluster/Cluster.java
@@ -87,9 +87,9 @@ public interface Cluster {
   }
 
   /**
-   * Returns local listen {@link Address} of this cluster instance.
+   * Returns {@link Address} of this cluster instance.
    *
-   * @return local server address
+   * @return cluster address
    */
   Address address();
 
