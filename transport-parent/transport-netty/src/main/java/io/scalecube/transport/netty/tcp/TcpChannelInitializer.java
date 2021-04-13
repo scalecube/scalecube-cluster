42ef370db464df7263ab32c11cd28762cb4aac5e
Artem Vysochyn <artem.vysochyn@gmail.com>
2021-04-13 12:03:48 +0300

    Removed TcpChannelInitializer.ExceptionHandler

--- transport-parent/transport-netty/src/main/java/io/scalecube/transport/netty/tcp/TcpChannelInitializer.java
+++ transport-parent/transport-netty/src/main/java/io/scalecube/transport/netty/tcp/TcpChannelInitializer.java
@@ -1,15 +1,10 @@
 package io.scalecube.transport.netty.tcp;
 
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelDuplexHandler;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
 import java.util.function.BiConsumer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.netty.ConnectionObserver;
 
 final class TcpChannelInitializer implements BiConsumer<ConnectionObserver, Channel> {
@@ -29,17 +24,5 @@ final class TcpChannelInitializer implements BiConsumer<ConnectionObserver, Chan
         new LengthFieldBasedFrameDecoder(
             maxFrameLength, 0, LENGTH_FIELD_LENGTH, 0, LENGTH_FIELD_LENGTH));
     pipeline.addFirst(new LengthFieldPrepender(LENGTH_FIELD_LENGTH));
-    pipeline.addLast(new ExceptionHandler());
-  }
-
-  @ChannelHandler.Sharable
-  static final class ExceptionHandler extends ChannelDuplexHandler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
-
-    @Override
-    public final void exceptionCaught(ChannelHandlerContext ctx, Throwable ex) {
-      LOGGER.warn("Exception caught on channel {}, cause: {}", ctx.channel(), ex.toString());
-    }
   }
 }
