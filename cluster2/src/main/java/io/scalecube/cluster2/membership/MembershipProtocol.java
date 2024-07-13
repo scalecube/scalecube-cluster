a21f5668dcf832b856e4809de2ed3d982ebae66e
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-07-13 20:26:26 +0300

    WIP

--- cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
+++ cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
@@ -1,12 +1,16 @@
 package io.scalecube.cluster2.membership;
 
 import static io.scalecube.cluster2.ShuffleUtil.shuffle;
+import static io.scalecube.cluster2.sbe.MemberActionType.ADD_MEMBER;
+import static io.scalecube.cluster2.sbe.MemberStatus.ALIVE;
 
 import io.scalecube.cluster.transport.api2.Transport;
 import io.scalecube.cluster2.AbstractAgent;
 import io.scalecube.cluster2.Member;
+import io.scalecube.cluster2.MemberActionCodec;
 import io.scalecube.cluster2.MemberCodec;
 import io.scalecube.cluster2.sbe.FailureDetectorEventDecoder;
+import io.scalecube.cluster2.sbe.MemberStatus;
 import io.scalecube.cluster2.sbe.MembershipRecordDecoder;
 import io.scalecube.cluster2.sbe.MessageHeaderDecoder;
 import io.scalecube.cluster2.sbe.SyncAckDecoder;
@@ -14,9 +18,13 @@ import io.scalecube.cluster2.sbe.SyncDecoder;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 import org.agrona.MutableDirectBuffer;
+import org.agrona.collections.Object2ObjectHashMap;
 import org.agrona.concurrent.EpochClock;
 import org.agrona.concurrent.broadcast.BroadcastTransmitter;
 import org.agrona.concurrent.broadcast.CopyBroadcastReceiver;
@@ -24,6 +32,7 @@ import org.agrona.concurrent.broadcast.CopyBroadcastReceiver;
 public class MembershipProtocol extends AbstractAgent {
 
   private final MembershipConfig config;
+  private final MembershipRecord localRecord;
   private final Member localMember;
 
   private final MessageHeaderDecoder headerDecoder = new MessageHeaderDecoder();
@@ -33,12 +42,15 @@ public class MembershipProtocol extends AbstractAgent {
       new FailureDetectorEventDecoder();
   private final MembershipRecordDecoder membershipRecordDecoder = new MembershipRecordDecoder();
   private final MemberCodec memberCodec = new MemberCodec();
+  private final MemberActionCodec memberActionCodec = new MemberActionCodec();
+  private final MembershipRecordCodec membershipRecordCodec = new MembershipRecordCodec();
   private final String roleName;
   private long period = 0;
   private final MemberSelector memberSelector;
   private List<String> seedMembers;
   private final ArrayList<Member> remoteMembers = new ArrayList<>();
   private final ArrayList<Member> nonSeedMembers = new ArrayList<>();
+  private final Map<UUID, MembershipRecord> recordMap = new Object2ObjectHashMap<>();
 
   public MembershipProtocol(
       Transport transport,
@@ -46,7 +58,7 @@ public class MembershipProtocol extends AbstractAgent {
       Supplier<CopyBroadcastReceiver> messageRxSupplier,
       EpochClock epochClock,
       MembershipConfig config,
-      Member localMember) {
+      MembershipRecord localRecord) {
     super(
         transport,
         messageTx,
@@ -55,10 +67,12 @@ public class MembershipProtocol extends AbstractAgent {
         null,
         Duration.ofMillis(config.syncInterval()));
     this.config = config;
-    this.localMember = localMember;
+    this.localRecord = localRecord;
+    this.localMember = localRecord.member();
     roleName = "membership@" + localMember.address();
     seedMembers = config.seedMembers();
     memberSelector = new MemberSelector(seedMembers, remoteMembers, nonSeedMembers);
+    recordMap.put(localMember.id(), localRecord);
   }
 
   @Override
@@ -118,6 +132,58 @@ public class MembershipProtocol extends AbstractAgent {
   private void onFailureDetectorEvent(FailureDetectorEventDecoder decoder) {}
 
   private void onMembershipRecord(MembershipRecordDecoder decoder) {}
+
+  private void putRecord(MembershipRecord record) {
+    final Member member = record.member();
+    final UUID key = member.id();
+    final MembershipRecord oldRecord = recordMap.get(key);
+    if (oldRecord == null) {
+      recordMap.put(key, record);
+      emitAddMember(member);
+      return;
+    }
+
+    if (record.incarnation() < oldRecord.incarnation()) {
+      return;
+    }
+
+    if (localMember.equals(member) && record.status() != ALIVE) {
+      localRecord.incarnation(localRecord.incarnation() + 1);
+      emitGossip(localRecord);
+      return;
+    }
+
+    if (record.incarnation() > oldRecord.incarnation()) {
+      recordMap.put(key, record);
+      return;
+    }
+
+    if (record.status() != ALIVE) {
+      recordMap.put(key, record);
+    }
+  }
+
+  private void putRecord(Member member, MemberStatus status) {
+    final MembershipRecord record = recordMap.get(member.id());
+    if (record != null && record.status() != status) {
+      record.status(status);
+      emitGossip(record);
+    }
+  }
+
+  private void forEach(Consumer<MembershipRecord> consumer) {
+    recordMap.values().forEach(consumer);
+  }
+
+  private void emitAddMember(final Member member) {
+    messageTx.transmit(
+        1, memberActionCodec.encode(ADD_MEMBER, member), 0, memberActionCodec.encodedLength());
+  }
+
+  private void emitGossip(MembershipRecord record) {
+    messageTx.transmit(
+        1, membershipRecordCodec.encode(record), 0, membershipRecordCodec.encodedLength());
+  }
 
   static class MemberSelector {
 
