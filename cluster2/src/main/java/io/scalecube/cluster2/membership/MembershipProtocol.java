a947002c9ee4309ebfd4978cbcfad7b87171210f
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-07-15 23:55:43 +0300

    WWIP

--- cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
+++ cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
@@ -140,13 +140,9 @@ public class MembershipProtocol extends AbstractAgent {
   }
 
   private void onSyncAck(SyncAckDecoder decoder) {
-    final long period = decoder.period();
-
-    if (this.period.get() != period) {
-      return;
+    if (period.get() == decoder.period()) {
+      membershipTable.put(membershipRecordCodec.membershipRecord(decoder::wrapMembershipRecord));
     }
-
-    membershipTable.put(membershipRecordCodec.membershipRecord(decoder::wrapMembershipRecord));
   }
 
   private void onGossipInputMessage(GossipInputMessageDecoder decoder) {
