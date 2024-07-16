54237d2a0ae689beec236f8b55241d00afbacd70
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-07-16 17:28:13 +0300

    WIP

--- cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
+++ cluster2/src/main/java/io/scalecube/cluster2/membership/MembershipProtocol.java
@@ -208,7 +208,7 @@ public class MembershipProtocol extends AbstractAgent {
     }
 
     Member nextRemoteMember() {
-      // TODO: implement more effecient way to find remoteMember that wouldn't be in seedMembers
+      // TODO: implement more effecient way to find correct remoteMember
 
       nonSeedMembers.clear();
 
