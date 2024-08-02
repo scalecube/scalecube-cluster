8ecdcb1ca087f366be4508623ac9469649ba7b96
Artem Vysochyn <artem.vysochyn@gmail.com>
2024-08-02 17:19:03 +0300

    WIP

--- cluster2/src/main/java/io/scalecube/cluster2/payload/PayloadStore.java
+++ cluster2/src/main/java/io/scalecube/cluster2/payload/PayloadStore.java
@@ -3,9 +3,10 @@ package io.scalecube.cluster2.payload;
 import static io.scalecube.cluster2.UUIDCodec.encodeUUID;
 
 import io.scalecube.cluster2.sbe.PayloadGenerationHeaderEncoder;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
 import java.util.UUID;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.MutableDirectBuffer;
@@ -14,26 +15,42 @@ import org.agrona.collections.Object2LongHashMap;
 public class PayloadStore {
 
   private final PayloadGenerationHeaderEncoder headerEncoder = new PayloadGenerationHeaderEncoder();
-  private final Object2LongHashMap<UUID> payloadIndex = new Object2LongHashMap<>(Long.MIN_VALUE);
   private final MutableDirectBuffer headerBuffer = new ExpandableArrayBuffer();
-  private FileChannel fileChannel;
-  private RandomAccessFile file;
+  private final Object2LongHashMap<UUID> payloadIndex = new Object2LongHashMap<>(Long.MIN_VALUE);
+  private RandomAccessFile payloadStore;
 
-  public PayloadStore() {}
+  public PayloadStore(File payloadFile) {
+    try {
+      payloadStore = new RandomAccessFile(payloadFile, "rw");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public void addGeneration(UUID memberId, int payloadLength) throws IOException {
-    payloadIndex.put(memberId, fileChannel.position());
+    payloadIndex.put(memberId, payloadStore.getFilePointer());
 
     headerEncoder.wrap(headerBuffer, 0);
     encodeUUID(memberId, headerEncoder.memberId());
     headerEncoder.payloadLength(payloadLength).payloadOffset(0);
 
-    file.write(headerBuffer.byteArray());
+    payloadStore.write(headerBuffer.byteArray());
 
     for (int i = 0; i < payloadLength; i++) {
-      file.write(0);
+      payloadStore.write(0);
     }
   }
 
-  public void removeGeneration(UUID memberId) {}
+  public void removeGeneration(UUID memberId) throws IOException {
+    final long position = payloadIndex.getValue(memberId);
+    if (position != Long.MIN_VALUE) {
+      payloadStore.read(headerBuffer.byteArray());
+      headerEncoder.wrap(headerBuffer, 0);
+      headerEncoder.payloadOffset(Integer.MIN_VALUE);
+
+      payloadStore.write(headerBuffer.byteArray());
+
+      payloadIndex.removeKey(memberId);
+    }
+  }
 }
---
+++ cluster2/src/test/java/io/scalecube/cluster2/payload/PayloadStoreTest.java
@@ -0,0 +1,23 @@
+package io.scalecube.cluster2.payload;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class PayloadStoreTest {
+
+  @TempDir private Path tempDir;
+
+  @Test
+  void testAddGeneration() throws IOException {
+    final PayloadStore payloadStore =
+        new PayloadStore(tempDir.resolve("" + System.currentTimeMillis()).toFile());
+    for (int i = 0; i < 3; i++) {
+      payloadStore.addGeneration(UUID.randomUUID(), 1024);
+    }
+  }
+}
